17b4b2dbf90cd67857f574d3c8c4cd01bad26b90
Kor <korostin.nikita@git.dev.gkomega.ru>
2021-10-28 19:11:21 +0300

    Add login_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,4 +1,8 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK = (By.ID, "login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.ID, "login_form")
+    REGISTRATION_FORM = (By.ID, "register_form")
---
+++ pages/login_page.py
@@ -0,0 +1,18 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "URL doesn't contain the word 'login'"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not present"
