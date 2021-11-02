d1b395d4558134b51075ba2d2305be97607187f6
Kor <korostin.nikita@git.dev.gkomega.ru>
2021-11-02 12:09:07 +0300

    Add login tests to product page

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,8 @@
 from .base_page import BasePage
 from .locators import ProductPageLocators
 from selenium.webdriver.common.by import By
+import math
+from selenium.common.exceptions import NoAlertPresentException
 
 class ProductPage(BasePage):
     def should_be_add_to_cart_button(self):
@@ -32,3 +34,17 @@ class ProductPage(BasePage):
         cart_total = self.browser.find_element(*ProductPageLocators.ADD_SUCCESS_CART_TOTAL).text
         item_price = self.browser.find_element(*ProductPageLocators.ITEM_PRICE).text
         assert cart_total == item_price, "Cart total is different from item price"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,5 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -17,3 +18,17 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.add_item_to_cart()
     page.solve_quiz_and_get_code()
     page.should_be_messages()
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
