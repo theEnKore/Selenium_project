db461e8ac68ec30e7941349c576011b33d1d4760
Kor <korostin.nikita@git.dev.gkomega.ru>
2021-11-03 19:41:42 +0300

    Add tests

--- test_main_page.py
+++ test_main_page.py
@@ -4,17 +4,19 @@ from .pages.basket_page import BasketPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
-def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
+    def test_guest_can_go_to_login_page(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
 
 def test_guest_should_see_basket_link(browser):
     page = MainPage(browser, link)
@@ -27,4 +29,3 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
-    
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.basket_page import BasketPage
+from .pages.locators import ProductPageLocators
 import pytest
 
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
@@ -33,6 +34,25 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_cart()
+    assert page.is_not_element_present(*ProductPageLocators.ADD_SUCCESS_ITEM), "Item added to cart message is not present, but should be"
+
+def test_guest_cant_see_success_message(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    assert page.is_not_element_present(*ProductPageLocators.ADD_SUCCESS_ITEM), "Item added to cart message is present, but shouldn't"
+    
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_item_to_cart()
+    assert page.is_disappeared(*ProductPageLocators.ADD_SUCCESS_ITEM), "Item added to cart message disappeared, but shouldn't"
 
 def test_guest_should_see_basket_link_on_product_page(browser):
     page = ProductPage(browser, link)
